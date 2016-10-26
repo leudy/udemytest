bdf43a226a6c72bb2233308d7e0ce40e04d10484
leudyrosario <eljevo13@gmail.com>
2016-10-26 11:30:44 -0400

    InicialComint

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hola Mundo");
+            ReadKey(true);
+        }
+    }
+}
